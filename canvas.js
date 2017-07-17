310d9be29741698bdc64f85ab7a8378a289d7d39
christikaes <christikaes@gmail.com>
2017-07-17 12:04:05 -0400

    add undo redo

--- canvas.js
+++ canvas.js
@@ -1,10 +1,14 @@
 (() => {
-    var canvas = new fabric.Canvas('canvas');
+    let past = [];
+    let present = {};
+    let future = [];
+
+    const canvas = new fabric.Canvas('canvas');
 
     canvas.setHeight(400);
     canvas.setWidth(600);
 
-    var rect = new fabric.Rect({
+    let rect = new fabric.Rect({
         left: 100,
         top: 100,
         fill: 'red',
@@ -14,4 +18,28 @@
 
     canvas.add(rect);
 
+    canvas.on("object:modified", () => {
+        past.push(present);
+        present = {...canvas.toJSON()};
+    })
+
+    const undo = () => {
+        future.push(present);
+        present = past.pop();
+        canvas.loadFromJSON(present);
+    }
+
+    const redo = () => {
+        past.push(present);
+        present = future.pop();
+        canvas.loadFromJSON(present)
+    }
+
+    window.control = {
+        undo,
+        redo,
+        past,
+        present,
+        future
+    };
 })();
